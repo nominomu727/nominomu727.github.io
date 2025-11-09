1aadff487491afa089337fd365aa9471c91e2f63
nominomu727 <flareling22@gmail.com>
2025-11-09 20:04:11 +1100

    Add files via upload

---
+++ files/files3.py
@@ -0,0 +1,6 @@
+n = 1443197936807647 # これは2つの素数の積
+sqrt = n ** 0.5 + 1
+for i in range(2, int(square_root)):
+    if n % i = 0:
+        print("{" + str(i) + " * " + str(int(n / i)) + " = " + n + "}")
+        break
